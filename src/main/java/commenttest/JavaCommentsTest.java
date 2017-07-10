25a3c27253b531f17015aaaf9cd0746e8b8c2bb1
idexche <userzi5j1u@some.mail>
2017-07-10 23:19:07 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1499636854182
+//first_change1499723489314
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
