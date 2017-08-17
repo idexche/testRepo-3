647286d759a13674a3f9e50d5e176cdcd5a868ee
idexche <useriyunlg@some.mail>
2017-08-17 06:11:35 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1502927202134
+//first_change1502943288000
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
