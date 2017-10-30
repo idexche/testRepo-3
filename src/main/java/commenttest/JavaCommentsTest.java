088d0fb4e80d3b34405034277c89f04c907f5448
idexche <che@eclipse.org>
2017-10-30 21:56:14 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1509380217502
+//first_change1509395044344
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
